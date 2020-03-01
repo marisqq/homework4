55056e20548f5e6a6e4523173041f923c058be98
Maris <marisvitolsm@gmail.com>
2020-03-01 14:08:26 +0200

    added fuctionality

--- index.php
+++ index.php
@@ -1,82 +1,71 @@
+<?php ?>
 <!DOCTYPE html>
-<?php include_once 'db.php';
-$sql  = "select * FROM tasks";
-$rows = $db->query($sql);
-?>
 <html lang="en">
-
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Coffee v.4.0.</title>
-  <script src="lib/jquery-3.4.1.min.js" defer></script>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="lib/jquery-3.4.1.min.js" defer></script>
   <script src="lib/bootstrap.min.js" defer></script>
+  <!-- <script src="custom.js" defer></script> -->
   <link rel="stylesheet" href="lib/bootstrap.min.css">
+  <link rel="stylesheet" href="css/style.css">
+    <title>To do</title>
 </head>
-
 <body>
-  <div class="container">
+<div class="row d-flex justify-content-center">
+  <div class="col-sm-4">
+    <div class="card m-5 bg-success">
+      <div class="card-body">
+      <h5 class="card-title text-white">Create task</h5>
+      <form method="POST" class="form-inline" action="add_query.php">
+					<input type="text" class="form-control" name="task">
 
-    <center>
-      <h1>Todo list</h1>
-    </center>
-  
-  <!-- Button trigger modal -->
-<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
-  Add task
-</button>
-
-<!-- Modal -->
-<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Add task</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-body">
-       
-        <form method="post" action="add.php">
-          <div class="form-group">
-            <label>Task name</label>
-            <input type="text" required name="task" class="form-control" class="btn btn-dark">
-          </div>
-          <input type="submit" name="send" value="+">
-        </form>
-      </div>
-      
-      <div class="modal-footer">
-     
+					<button class="btn btn-primary buttn ml-2" name="add">Add Task</button>
+				</form>
       </div>
     </div>
   </div>
-</div>
+
+<div class="row">
+  <div class="col-sm-14">
+    <div class="card m-3 mt-4">
+      <div class="card-body">
+      <table class="table">
+				<tr>
+					<th>#</th>
+					<th>Task</th>
+					<th>Status</th>
+					<th>Action</th>
+				</tr>
+			<tbody>
+				<?php
+require 'conn.php';
+$query = $conn->query("SELECT * FROM `task` ORDER BY `task_id` ASC");
+$count = 1;
+while ($fetch = $query->fetch_array()) {
+    ?>
+				<tr>
+					<td><?php echo $count++ ?></td>
+					<td><?php echo $fetch['task'] ?></td>
+					<td><?php echo $fetch['status'] ?></td>
+					<td colspan="2">
+
+							<?php
+if ($fetch['status'] != "Done") {
+        echo '<a href="update_task.php?task_id=' . $fetch['task_id'] . '" class="btn btn-success"><span> ✔ </span></a> ';
+    }?>
+	 <a href="delete_query.php?task_id=<?php echo $fetch['task_id'] ?>" class="btn btn-danger"><span>✖</span></a>
+					</td>
+				</tr>
+				<?php
+}
+?>
+			</tbody>
+		</table>
+      </div>
+    </div>
   </div>
-  <hr>
-  <table class="table">
-    <thead>
-      <tr>
-        <th scope="col">ID</th>
-        <th scope="col">Task</th>
 
-
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
-        <?php while($row = $rows->fetch_assoc()): ?>
-
-        <th> <?php echo $row['id']?> </th>
-        <th> <?php echo $row['name']?> </th>
-        <td><a href="update.php?id=<?php echo $row['id'] ?>" class="btn btn-success">Edit</a></td>
-        <td><a href="delete.php?id=<?php echo $row['id'] ?>" class="btn btn-danger">Delete</a></td>
-      </tr>
-      <?php endwhile; ?>
-    </tbody>
-  </table>
 
 </body>
-
 </html>
